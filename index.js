945aadbc9e298b65f6c3206b86ed703bfeec5380
Ian <imacmumf@gmail.com>
2021-06-27 13:36:42 -0500

    Add more mobile config

--- index.js
+++ index.js
@@ -23,15 +23,17 @@
   let time = date.getHours();
 
 
-  $(".clock").html(dayName[dayNum] + " " + d + "-" + month + "-" + year);
+$(".clock").html(dayName[dayNum] + " " + d + "-" + month + "-" + year);
 
-if(time < 12 ){
-  $(".time-greeting").html("Good Morning!");
-} else if (time >= 12 && time < 18){
-  $(".time-greeting").html("Good Afternoon!");
-} else if (time >= 18 && time <= 24){
-  $(".time-greeting").html("Good Evening!");
-};
+// if(time < 12 ){
+//   $(".time-greeting").html("Good Morning!");
+// } else if (time >= 12 && time < 18){
+//   $(".time-greeting").html("Good Afternoon!");
+// } else if (time >= 18 && time <= 24){
+//   $(".time-greeting").html("Good Evening!");
+// };
+
+
 
 
 //Fade out background image
