2a524fddd1191681316cabaa880ba37bdd10b3f3
Ian <imacmumf@gmail.com>
2021-06-29 10:52:22 -0500

    Js Greetings

--- index.js
+++ index.js
@@ -26,11 +26,11 @@
   $(".clock").html(dayName[dayNum] + " " + d + "-" + month + "-" + year);
 
 if(time < 12 ){
-  $(".time-greeting").html("Good Morning!");
+  $(".time-greeting").html("Good Morning");
 } else if (time >= 12 && time < 18){
-  $(".time-greeting").html("Good Afternoon!");
+  $(".time-greeting").html("Good Afternoon");
 } else if (time >= 18 && time <= 24){
-  $(".time-greeting").html("Good Evening!");
+  $(".time-greeting").html("Good Evening");
 };
 
 
